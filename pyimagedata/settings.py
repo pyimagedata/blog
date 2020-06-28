fffbf09df633b09f0f362d93d6edefbd882e7731
= <=>
2020-06-28 20:58:18 +0300

    allow host

--- pyimagedata/settings.py
+++ pyimagedata/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!@*e1&ryr^x3eo%e81u@3sf^!y%%mg=s&*y#pscp&x+gzcva5x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['muaewsomeapp.herokuapp.com']
+ALLOWED_HOSTS = ['djangopyimage.herokuapp.com']
 
 
 # Application definition
